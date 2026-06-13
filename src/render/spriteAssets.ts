a676c876fae7b32f63de8038e6c3ae6887b4bccc
sucRo0629 <mi051335@gmail.com>
2026-06-13 11:41:14 +0900

    不要アイコン削除

--- src/render/spriteAssets.ts
+++ src/render/spriteAssets.ts
@@ -1,21 +1,19 @@
-import defenderPlaceholderUrl from '../assets/sprites/defender_placeholder.png';
-import attackerMeleePlaceholderUrl from '../assets/sprites/attacker_melee_placeholder.png';
-import attackerRangedPlaceholderUrl from '../assets/sprites/attacker_ranged_placeholder.png';
-import supporterPlaceholderUrl from '../assets/sprites/supporter_placeholder.png';
-import slimeUrl from '../assets/sprites/slime.png';
-import enemyDefaultUrl from '../assets/sprites/enemy_default.png';
-import { PLACEHOLDER_SPRITE_KEYS } from '../battle/classVisuals.ts';
-import { hasSpriteSheetKey } from './spriteSheetRegistry.ts';
+import defenderPlaceholderUrl from "../assets/sprites/defender_placeholder.png";
+import attackerMeleePlaceholderUrl from "../assets/sprites/attacker_melee_placeholder.png";
+import attackerRangedPlaceholderUrl from "../assets/sprites/attacker_ranged_placeholder.png";
+import supporterPlaceholderUrl from "../assets/sprites/supporter_placeholder.png";
+import enemyDefaultUrl from "../assets/sprites/enemy_default.png";
+import { PLACEHOLDER_SPRITE_KEYS } from "../battle/classVisuals.ts";
+import { hasSpriteSheetKey } from "./spriteSheetRegistry.ts";
 
-export const ENEMY_DEFAULT_SPRITE_KEY = 'enemy_default';
+export const ENEMY_DEFAULT_SPRITE_KEY = "enemy_default";
 
 const LEGACY_SPRITE_KEYS = new Set<string>([
   ...Object.values(PLACEHOLDER_SPRITE_KEYS),
-  'defender_placeholder',
-  'attacker_melee_placeholder',
-  'attacker_ranged_placeholder',
-  'supporter_placeholder',
-  'slime',
+  "defender_placeholder",
+  "attacker_melee_placeholder",
+  "attacker_ranged_placeholder",
+  "supporter_placeholder",
   ENEMY_DEFAULT_SPRITE_KEY,
 ]);
 
@@ -24,19 +22,19 @@ export const SPRITE_URLS: Record<string, string> = {
   [PLACEHOLDER_SPRITE_KEYS.supporter]: supporterPlaceholderUrl,
   [PLACEHOLDER_SPRITE_KEYS.attackerGeneral]: attackerMeleePlaceholderUrl,
   [PLACEHOLDER_SPRITE_KEYS.attackerMelee]: attackerMeleePlaceholderUrl,
-  [PLACEHOLDER_SPRITE_KEYS.attackerRangedPhysical]: attackerRangedPlaceholderUrl,
+  [PLACEHOLDER_SPRITE_KEYS.attackerRangedPhysical]:
+    attackerRangedPlaceholderUrl,
   [PLACEHOLDER_SPRITE_KEYS.attackerRangedMagic]: attackerRangedPlaceholderUrl,
   defender_placeholder: defenderPlaceholderUrl,
   attacker_melee_placeholder: attackerMeleePlaceholderUrl,
   attacker_ranged_placeholder: attackerRangedPlaceholderUrl,
   supporter_placeholder: supporterPlaceholderUrl,
-  slime: slimeUrl,
   [ENEMY_DEFAULT_SPRITE_KEY]: enemyDefaultUrl,
 };
 
 const entitySpriteModules = import.meta.glob<string>(
-  '../assets/sprites/*.png',
-  { eager: true, import: 'default' },
+  "../assets/sprites/*.png",
+  { eager: true, import: "default" }
 );
 
 for (const [path, url] of Object.entries(entitySpriteModules)) {
